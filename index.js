ba44a3a6249e319c4b68c876f8829844eb898350
derrick b <dbozich1@gmail.com>
2018-09-24 02:09:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //Your code here
+function justInvoke(fn){
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arg){
+  return result = fn.call(thisValue, arg);
+}
+
+function setThisWithApply(fn, thisValue, args){
+  return result = fn.apply(thisValue, args);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  let copy = functionToBeCopied.bind(thisValue);
+  return copy;
+}
+
+// function (){ return {thisValue: this, arguments: Array.from(arguments)} }
